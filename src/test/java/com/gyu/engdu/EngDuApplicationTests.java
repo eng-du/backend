90074664e70485a4d1aba7df2697574feeee3ff2
young999999999 <ssas146310@gmail.com>
2025-10-15 03:20:55 +0900

    first commit

---
+++ src/test/java/com/gyu/engdu/EngDuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gyu.engdu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EngDuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
