31cfb74ddbb5cf7035393613e1428678441c4faa
young999999999 <ssas146310@gmail.com>
2025-10-17 01:47:24 +0900

    test: jwt 토큰 발급, 파싱 로직 단위 테스트 작성

---
+++ src/test/java/com/gyu/engdu/EngDuApplicationTests.java
@@ -0,0 +1,12 @@
+package com.gyu.engdu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EngDuApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+}
