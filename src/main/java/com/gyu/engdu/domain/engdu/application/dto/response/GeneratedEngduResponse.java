55114e1f5b283deb98612ae91b420f7011757071
young999999999 <ssas146310@gmail.com>
2025-11-08 16:00:51 +0900

    feat: LLM API 요청을 위한 잉듀 클라이언트 구현

--- src/main/java/com/gyu/engdu/domain/engdu/application/dto/response/GeneratedEngduResponse.java
+++ src/main/java/com/gyu/engdu/domain/engdu/application/dto/response/GeneratedEngduResponse.java
@@ -1,36 +1,54 @@
 package com.gyu.engdu.domain.engdu.application.dto.response;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.gyu.engdu.domain.engdu.domain.Article;
+import com.gyu.engdu.domain.engdu.domain.Choice;
+import com.gyu.engdu.domain.engdu.domain.Engdu;
+import com.gyu.engdu.domain.engdu.domain.Question;
+import com.gyu.engdu.domain.engdu.domain.enums.Category;
 import java.util.List;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 public record GeneratedEngduResponse(
     String title,
-    List<ArticleDto> articles
+    List<ArticleDto> articles,
+    List<QuestionDto> questions
 ) {
 
   public record ArticleDto(
       String content,
-      String translation,
-
-      List<QuizDto> quizzes
+      String translation
   ) {
+
+    public Article toEntity(Engdu engdu) {
+      return Article.of(this.content, this.translation, engdu);
+    }
 
   }
 
-  public record QuizDto(
-      String question,
+  public record QuestionDto(
+      String content,
       String type,
       List<ChoiceDto> choices,
-      Integer answer
+      Byte answer
   ) {
+
+    public Question toEntity(Engdu engdu) {
+      Category category = Category.valueOf(type);
+      return Question.of(this.answer, this.content, category, engdu);
+    }
 
   }
 
   public record ChoiceDto(
+
+      Byte seq,
       String content,
       String explanation
   ) {
 
+    public Choice toEntity(Question question) {
+      return Choice.of(this.content, this.explanation, this.seq, question);
+    }
   }
 }
