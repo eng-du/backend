3c7d4683e7ebe7af8dd3a8113eb0cac1740ea62c
young999999999 <ssas146310@gmail.com>
2025-11-08 22:00:06 +0900

    fix: REST에 맞게 Pathvariable로 잉듀를 식별하도록 변경

--- src/main/java/com/gyu/engdu/domain/engdu/presentation/EngduController.java
+++ src/main/java/com/gyu/engdu/domain/engdu/presentation/EngduController.java
@@ -7,6 +7,7 @@ import java.net.URI;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -31,9 +32,8 @@ public class EngduController {
     return ResponseEntity.created(location).build();
   }
 
-  @DeleteMapping
-  public ResponseEntity<Void> deleteEngdu(@RequestBody DeleteEngduRequest request) {
-    Long engduId = request.engduId();
+  @DeleteMapping("/{engduId}")
+  public ResponseEntity<Void> deleteEngdu(@PathVariable("engduId") Long engduId) {
     deleteEngduService.delete(1L, engduId);
     return ResponseEntity.noContent().build();
   }
