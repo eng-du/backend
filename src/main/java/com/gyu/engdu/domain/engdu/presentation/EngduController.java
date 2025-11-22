248f201d0a530a0c886497196c61f176b69e2f23
young999999999 <ssas146310@gmail.com>
2025-11-22 20:48:52 +0900

    feat: 잉듀 페이징을 위한 컨트롤러 구현
    - 인증 로직이 구현되지 않았으므로 임시적으로 userId를 1로 하드코딩한다.

--- src/main/java/com/gyu/engdu/domain/engdu/presentation/EngduController.java
+++ src/main/java/com/gyu/engdu/domain/engdu/presentation/EngduController.java
@@ -2,15 +2,23 @@ package com.gyu.engdu.domain.engdu.presentation;
 
 import com.gyu.engdu.domain.engdu.application.CreateEngduService;
 import com.gyu.engdu.domain.engdu.application.DeleteEngduService;
+import com.gyu.engdu.domain.engdu.application.EngduQueryService;
+import com.gyu.engdu.domain.engdu.domain.enums.EngduSortKey;
+import com.gyu.engdu.domain.engdu.domain.enums.SolvedFilter;
 import com.gyu.engdu.domain.engdu.presentation.dto.request.CreateEngduRequest;
+import com.gyu.engdu.domain.engdu.presentation.dto.response.EngduSummaryResponse;
 import java.net.URI;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -20,6 +28,7 @@ public class EngduController {
 
   private final CreateEngduService createEngduService;
   private final DeleteEngduService deleteEngduService;
+  private final EngduQueryService engduQueryService;
 
   @PostMapping
   public ResponseEntity<Void> createEngdu(@RequestBody CreateEngduRequest request) {
@@ -36,5 +45,19 @@ public class EngduController {
   public ResponseEntity<Void> deleteEngdu(@PathVariable("engduId") Long engduId) {
     deleteEngduService.delete(1L, engduId);
     return ResponseEntity.noContent().build();
+  }
+
+  @GetMapping
+  public ResponseEntity<Page<EngduSummaryResponse>> searchEngdu(
+      @RequestParam(name = "page", defaultValue = "1") Integer pageNum,
+      @RequestParam(name = "size", defaultValue = "6") Integer size,
+      @RequestParam(name = "sortKey", defaultValue = "CREATED_AT") EngduSortKey sortKey,
+      @RequestParam(name = "direction", defaultValue = "DESC") Sort.Direction direction,
+      @RequestParam(name = "isSolved", defaultValue = "ALL") SolvedFilter solvedFilter
+  ) {
+    Long userId = 1L;
+    Page<EngduSummaryResponse> responses = engduQueryService.searchEngdu(userId, pageNum, size,
+        sortKey, direction, solvedFilter);
+    return ResponseEntity.ok(responses);
   }
 }
