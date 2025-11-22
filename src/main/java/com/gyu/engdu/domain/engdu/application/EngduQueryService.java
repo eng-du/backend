35678c1fe06f60e08aade895ea76bf5d69f8be2d
young999999999 <ssas146310@gmail.com>
2025-11-22 20:48:04 +0900

    feat: 잉듀 페이징 서비스, 레파지토리 구현

--- src/main/java/com/gyu/engdu/domain/engdu/application/EngduQueryService.java
+++ src/main/java/com/gyu/engdu/domain/engdu/application/EngduQueryService.java
@@ -2,9 +2,16 @@ package com.gyu.engdu.domain.engdu.application;
 
 import com.gyu.engdu.domain.engdu.domain.Engdu;
 import com.gyu.engdu.domain.engdu.domain.EngduRepository;
+import com.gyu.engdu.domain.engdu.domain.enums.EngduSortKey;
+import com.gyu.engdu.domain.engdu.domain.enums.SolvedFilter;
+import com.gyu.engdu.domain.engdu.presentation.dto.response.EngduSummaryResponse;
 import com.gyu.engdu.exception.CustomException;
 import com.gyu.engdu.exception.ErrorCode;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -18,5 +25,28 @@ public class EngduQueryService {
   public Engdu findExistingEngdu(Long id) {
     return engduRepository.findById(id)
         .orElseThrow(() -> new CustomException(ErrorCode.ENGDU_NOT_FOUND));
+  }
+
+  public Page<EngduSummaryResponse> searchEngdu(Long userId, Integer pageNum, Integer size, EngduSortKey sortKey,
+      Sort.Direction direction, SolvedFilter solvedFilter) {
+    Pageable pageable = PageRequest.of(
+        pageNum,
+        size,
+        Sort.by(direction, sortKey.getProperty())
+    );
+
+    switch (solvedFilter){
+      case TRUE,FALSE -> {
+        return engduRepository
+            .findAllByUserIdAndIsAllSolved(userId, solvedFilter.getProperty(), pageable)
+            .map(EngduSummaryResponse::from);
+      }
+
+      default -> {
+        return engduRepository
+            .findALlByUserId(userId, pageable)
+            .map(EngduSummaryResponse::from);
+      }
+    }
   }
 }
--- src/main/java/com/gyu/engdu/domain/engdu/domain/EngduRepository.java
+++ src/main/java/com/gyu/engdu/domain/engdu/domain/EngduRepository.java
@@ -1,9 +1,14 @@
 package com.gyu.engdu.domain.engdu.domain;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface EngduRepository extends JpaRepository<Engdu, Long> {
 
+  Page<Engdu> findAllByUserIdAndIsAllSolved(Long userId, Boolean isAllSovled, Pageable pageable);
+
+  Page<Engdu> findALlByUserId(Long userId, Pageable pageable);
 }
