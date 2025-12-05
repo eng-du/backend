83f887ba6ef63ec371504a81cde55e309c454a3b
young999999999 <ssas146310@gmail.com>
2025-12-05 20:36:18 +0900

    feat: 인증 필터 구현

--- src/main/java/com/gyu/engdu/domain/auth/config/JwtTokenConfig.java
+++ src/main/java/com/gyu/engdu/domain/auth/config/JwtTokenConfig.java
@@ -10,7 +10,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class AuthConfig {
+public class JwtTokenConfig {
 
   @Value("${spring.jwt.secret-key}")
   private String secretKey;
