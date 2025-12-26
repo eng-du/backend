1ee72b1bf61a3eea37f95709d1e2290ef573cbac
young999999999 <ssas146310@gmail.com>
2025-12-26 13:41:22 +0900

    feat: 리프레시토큰에 발급 시간 필드 추가

--- src/main/java/com/gyu/engdu/domain/auth/infra/dto/response/GoogleUserInfo.java
+++ src/main/java/com/gyu/engdu/domain/auth/infra/dto/response/GoogleUserInfo.java
@@ -3,8 +3,10 @@ package com.gyu.engdu.domain.auth.infra.dto.response;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.gyu.engdu.domain.auth.application.dto.response.OAuthUserInfo;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 @Data
+@EqualsAndHashCode(callSuper = false)
 public class GoogleUserInfo extends OAuthUserInfo {
 
   private String id;
