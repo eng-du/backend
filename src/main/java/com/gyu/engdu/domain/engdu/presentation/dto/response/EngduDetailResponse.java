0eabacac3dcbf550d7bc8422ae5b189c3585ea3f
young999999999 <ssas146310@gmail.com>
2025-12-27 17:43:35 +0900

    feat: 응답 DTO에 engduId,questionId,answer 필드 추가
    - question의 isCorrected가 false라면 answer 필드는 null이 된다.

--- src/main/java/com/gyu/engdu/domain/engdu/presentation/dto/response/EngduDetailResponse.java
+++ src/main/java/com/gyu/engdu/domain/engdu/presentation/dto/response/EngduDetailResponse.java
@@ -6,56 +6,63 @@ import com.gyu.engdu.domain.engdu.domain.Question;
 import java.util.List;
 
 public record EngduDetailResponse(
-    String title,
-    String topic,
-    List<ArticleDto> articles,
-    List<QuestionDto> questions
-) {
+        Long engduId,
+        String title,
+        String topic,
+        List<ArticleDto> articles,
+        List<QuestionDto> questions) {
 
-  public static EngduDetailResponse fromEntity(Engdu engdu) {
-    String title = engdu.getTitle();
-    String topic = engdu.getTopic();
-    List<Article> articles = engdu.getArticles();
-    List<Question> questions = engdu.getQuestions();
+    public static EngduDetailResponse fromEntity(Engdu engdu) {
+        Long engduId = engdu.getId();
+        String title = engdu.getTitle();
+        String topic = engdu.getTopic();
+        List<Article> articles = engdu.getArticles();
+        List<Question> questions = engdu.getQuestions();
 
-    List<ArticleDto> articleDtos = articles.stream()
-        .map(article -> new ArticleDto(article.getContent(), article.getTranslation()))
-        .toList();
+        List<ArticleDto> articleDtos = articles.stream()
+                .map(article -> new ArticleDto(article.getContent(), article.getTranslation()))
+                .toList();
 
-    List<QuestionDto> questionDtos = questions.stream().map(question -> {
-      List<ChoiceDto> choices = question.getChoices().stream()
-          .map(choice -> new ChoiceDto(
-              choice.getSeq(),
-              choice.getContent(),
-              choice.getExplanation()
-          ))
-          .toList();
+        List<QuestionDto> questionDtos = questions.stream().map(question -> {
+            List<ChoiceDto> choices = question.getChoices().stream()
+                    .map(choice -> new ChoiceDto(
+                            choice.getSeq(),
+                            choice.getContent(),
+                            choice.getExplanation()))
+                    .toList();
 
-      return new QuestionDto(question.getContent(), choices);
-    }).toList();
+            Integer answer = question.isCorrected() ? (int) question.getAnswer() : null;
 
-    return new EngduDetailResponse(title, topic, articleDtos, questionDtos);
-  }
+            return new QuestionDto(
+                    question.getId(),
+                    question.getContent(),
+                    answer,
+                    question.isCorrected(),
+                    choices);
+        }).toList();
 
-  public record ArticleDto(
-      String content,
-      String translation
-  ) {
+        return new EngduDetailResponse(engduId, title, topic, articleDtos, questionDtos);
+    }
 
-  }
+    public record ArticleDto(
+            String content,
+            String translation) {
 
-  public record QuestionDto(
-      String content,
-      List<ChoiceDto> choices
-  ) {
+    }
 
-  }
+    public record QuestionDto(
+            Long questionId,
+            String content,
+            Integer answer,
+            boolean isCorrected,
+            List<ChoiceDto> choices) {
 
-  public record ChoiceDto(
-      Byte seq,
-      String content,
-      String explanation
-  ) {
+    }
 
-  }
+    public record ChoiceDto(
+            Byte seq,
+            String content,
+            String explanation) {
+
+    }
 }
