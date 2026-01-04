0c62250fc9f24be05ea3889a28d27291c867e281
Young999999999 <ssas146310@gmail.com>
2026-01-04 23:19:24 +0900

    feat: 사용자는 내 정보 조회 API를 사용할 수 있다.  (#45)
    * feat: 유저 정보 간단 조회 기능 및 응답 DTO 추가
    
    * feat: 유저 정보 간단 조회 API 추가
    
    * chore: [리뷰반영] response 패키지 오타 수정

--- src/main/java/com/gyu/engdu/domain/auth/application/LogoutService.java
+++ src/main/java/com/gyu/engdu/domain/auth/application/LogoutService.java
@@ -4,6 +4,7 @@ import com.gyu.engdu.domain.auth.domain.RefreshToken;
 import com.gyu.engdu.domain.auth.domain.RefreshTokenRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
