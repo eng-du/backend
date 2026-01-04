4c99d2c8f33fd351fc0cacc0f6d5c602d5e3f686
young999999999 <ssas146310@gmail.com>
2026-01-04 23:06:59 +0900

    feat: 유저 정보 간단 조회 API 추가

---
+++ src/main/java/com/gyu/engdu/domain/user/presentation/UserController.java
@@ -0,0 +1,25 @@
+package com.gyu.engdu.domain.user.presentation;
+
+import com.gyu.engdu.domain.user.application.UserQueryService;
+import com.gyu.engdu.domain.user.application.dto.reponse.UserSummaryResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/user")
+@RequiredArgsConstructor
+public class UserController {
+
+  private final UserQueryService userQueryService;
+
+  @GetMapping("/me")
+  public ResponseEntity<UserSummaryResponse> readMySummaryInfo(
+      @AuthenticationPrincipal(expression = "userId") Long userId
+  ) {
+    return ResponseEntity.ok(userQueryService.findSummaryUserInfo(userId));
+  }
+}
