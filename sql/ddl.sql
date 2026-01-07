7352a25a7cd4a7a9b88a54ce82cecb0e50a7cf3f
Young999999999 <ssas146310@gmail.com>
2026-01-07 22:38:14 +0900

    feat: 사용자는 회원 탈퇴하여 사용자 정보를 지울 수 있다. (#49)
    * feat: 유저 삭제 서비스 구현
    
    * feat: 회원탈퇴 API 구현
    
    * chore: 데이터베이스 ddl 작성 및 추가

---
+++ sql/ddl.sql
@@ -0,0 +1,83 @@
+
+-- **************************************** TABLE START ****************************************
+-- User Table
+CREATE TABLE `user` (
+    `user_id` BIGINT AUTO_INCREMENT PRIMARY KEY,
+    `created_at` DATETIME(6) DEFAULT CURRENT_TIMESTAMP(6),
+    `modified_at` DATETIME(6) DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6),
+    `email` VARCHAR(255),
+    `name` VARCHAR(255),
+    `role` VARCHAR(255),
+    `sub` VARCHAR(255),
+    CONSTRAINT `unique_sub` UNIQUE (`sub`)
+) ENGINE=InnoDB;
+
+-- RefreshToken Table
+CREATE TABLE `refresh_token` (
+     `refresh_token_id` BIGINT AUTO_INCREMENT PRIMARY KEY,
+     `created_at` DATETIME(6) DEFAULT CURRENT_TIMESTAMP(6),
+     `modified_at` DATETIME(6) DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6),
+     `raw_token` VARCHAR(255) NOT NULL,
+     `user_id` BIGINT
+) ENGINE=InnoDB;
+
+-- Engdu Table
+CREATE TABLE `engdu` (
+    `engdu_id` BIGINT AUTO_INCREMENT PRIMARY KEY,
+    `created_at` DATETIME(6) DEFAULT CURRENT_TIMESTAMP(6),
+    `modified_at` DATETIME(6) DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6),
+    `is_all_solved` BIT(1) NOT NULL,
+    `solved_count` INT NOT NULL,
+    `title` VARCHAR(255),
+    `topic` VARCHAR(255),
+    `user_id` BIGINT
+) ENGINE=InnoDB;
+
+-- Article Table
+CREATE TABLE `article` (
+    `article_id` BIGINT AUTO_INCREMENT PRIMARY KEY,
+    `created_at` DATETIME(6) DEFAULT CURRENT_TIMESTAMP(6),
+    `modified_at` DATETIME(6) DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6),
+    `engdu_id` BIGINT NOT NULL
+) ENGINE=InnoDB;
+
+-- ArticleChunk Table
+CREATE TABLE `article_chunk` (
+    `article_chunk_id` BIGINT AUTO_INCREMENT PRIMARY KEY,
+    `created_at` DATETIME(6) DEFAULT CURRENT_TIMESTAMP(6),
+    `modified_at` DATETIME(6) DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6),
+    `en` VARCHAR(2000),
+    `kor` VARCHAR(2000),
+    `article_id` BIGINT NOT NULL
+) ENGINE=InnoDB;
+
+-- Question Table
+CREATE TABLE `question` (
+    `question_id` BIGINT AUTO_INCREMENT PRIMARY KEY,
+    `created_at` DATETIME(6) DEFAULT CURRENT_TIMESTAMP(6),
+    `modified_at` DATETIME(6) DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6),
+    `answer` TINYINT NOT NULL,
+    `category` VARCHAR(255),
+    `content` VARCHAR(255),
+    `is_corrected` BIT(1) NOT NULL,
+    `engdu_id` BIGINT NOT NULL
+) ENGINE=InnoDB;
+
+-- Choice Table
+CREATE TABLE `choice` (
+    `choice_id` BIGINT AUTO_INCREMENT PRIMARY KEY,
+    `created_at` DATETIME(6) DEFAULT CURRENT_TIMESTAMP(6),
+    `modified_at` DATETIME(6) DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6),
+    `content` VARCHAR(255),
+    `explanation` VARCHAR(255),
+    `seq` TINYINT,
+    `question_id` BIGINT NOT NULL
+) ENGINE=InnoDB;
+-- **************************************** TABLE END****************************************
+
+-- **************************************** INDEX START ****************************************
+CREATE INDEX idx_article_engdu_id ON `article` (`engdu_id`);
+CREATE INDEX idx_article_chunk_article_id ON `article_chunk` (`article_id`);
+CREATE INDEX idx_question_engdu_id ON `question` (`engdu_id`);
+CREATE INDEX idx_choice_question_id ON `choice` (`question_id`);
+-- **************************************** INDEX END ****************************************
--- src/main/java/com/gyu/engdu/domain/user/presentation/UserController.java
+++ src/main/java/com/gyu/engdu/domain/user/presentation/UserController.java
@@ -1,10 +1,12 @@
 package com.gyu.engdu.domain.user.presentation;
 
+import com.gyu.engdu.domain.user.application.DeleteUserService;
 import com.gyu.engdu.domain.user.application.UserQueryService;
 import com.gyu.engdu.domain.user.application.dto.response.UserSummaryResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,11 +17,21 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
 
   private final UserQueryService userQueryService;
+  private final DeleteUserService deleteUserService;
 
   @GetMapping("/me")
   public ResponseEntity<UserSummaryResponse> readMySummaryInfo(
       @AuthenticationPrincipal(expression = "userId") Long userId
   ) {
     return ResponseEntity.ok(userQueryService.findSummaryUserInfo(userId));
+  }
+
+  @DeleteMapping("/withdrawal")
+  public ResponseEntity<Void> withdrawAccount(
+      @AuthenticationPrincipal(expression = "userId") Long userId
+  ) {
+    deleteUserService.delete(userId);
+    return ResponseEntity.noContent()
+        .build();
   }
 }
