d884bc1bf264137d3362ea00874352336c668047
young999999999 <ssas146310@gmail.com>
2026-02-12 23:47:39 +0900

    feat: 인증 토큰 응답 DTO 수정

--- src/main/java/com/gyu/engdu/domain/auth/presentation/dto/response/AuthTokenResponse.java
+++ src/main/java/com/gyu/engdu/domain/auth/presentation/dto/response/AuthTokenResponse.java
@@ -3,15 +3,13 @@ package com.gyu.engdu.domain.auth.presentation.dto.response;
 import com.gyu.engdu.domain.auth.application.dto.response.AuthTokenServiceResponse;
 
 public record AuthTokenResponse(
-    String accessToken,
-    String refreshToken
+    String accessToken
 ) {
 
   public static AuthTokenResponse from(AuthTokenServiceResponse authTokenServiceResponse) {
 
     return new AuthTokenResponse(
-        authTokenServiceResponse.accessToken().getRawToken(),
-        authTokenServiceResponse.refreshToken().getRawToken()
+        authTokenServiceResponse.accessToken().getRawToken()
     );
   }
 }
