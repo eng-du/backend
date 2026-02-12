76fd52e6d5e60766a49a3d16b6122299b1de2cb8
young999999999 <ssas146310@gmail.com>
2026-02-12 20:39:18 +0900

    refactor: 스프링 시큐리티 인증 예외 변경

--- src/main/java/com/gyu/engdu/security/CustomAuthenticationEntryPoint.java
+++ src/main/java/com/gyu/engdu/security/CustomAuthenticationEntryPoint.java
@@ -7,6 +7,8 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
+
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
@@ -22,8 +24,9 @@ public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint 
     ErrorCode errorCode = (ErrorCode) request.getAttribute("errorCode");
 
     if (errorCode != null) {
-      ErrorResponseEntity errorResponse = ErrorResponseEntity.of(errorCode);
-      response.setStatus(errorCode.getHttpStatus().value());
+      ErrorResponseEntity errorResponse = ErrorResponseEntity.of(errorCode,
+          HttpStatus.UNAUTHORIZED);
+      response.setStatus(HttpStatus.UNAUTHORIZED.value());
       response.setContentType(MediaType.APPLICATION_JSON_VALUE);
       response.setCharacterEncoding("UTF-8");
       response.getWriter().write(mapper.writeValueAsString(errorResponse));
