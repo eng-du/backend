376b64b27a57c5cf2f5920fd1a492b80b1da7cea
young999999999 <ssas146310@gmail.com>
2026-02-18 01:11:06 +0900

    fix: 액츄에이터 엔드포인트 로깅 수정

--- src/main/java/com/gyu/engdu/security/JwtAuthenticationFilter.java
+++ src/main/java/com/gyu/engdu/security/JwtAuthenticationFilter.java
@@ -32,7 +32,12 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
     // 로그인 관련 요청은 JWT 검증을 하지 않음
     String requestPath = request.getRequestURI();
-    log.info("{} {}", request.getMethod(), requestPath);
+
+    // 액츄에이터 엔드포인트는 로깅 제외
+    if (!requestPath.startsWith("/actuator")) {
+      log.info("{} {}", request.getMethod(), requestPath);
+    }
+
     if (requestPath.startsWith("/api/v1/login") || requestPath.startsWith("/favicon.ico")) {
       filterChain.doFilter(request, response);
       return;
