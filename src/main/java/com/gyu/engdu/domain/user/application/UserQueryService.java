1e730d5392edb83639c995048f172fb8039eae0b
Young999999999 <ssas146310@gmail.com>
2026-02-18 16:25:38 +0900

    [FEAT] 사용자는 마이페이지에서 사용자 정보를 조회할 수 있다. (#79)
    * feat: 사용자 정보 조회 서비스 구현
    
    * feat: 사용자 정보 조회 api 구현

--- src/main/java/com/gyu/engdu/domain/user/application/UserQueryService.java
+++ src/main/java/com/gyu/engdu/domain/user/application/UserQueryService.java
@@ -1,5 +1,6 @@
 package com.gyu.engdu.domain.user.application;
 
+import com.gyu.engdu.domain.user.application.dto.response.UserDetailResponse;
 import com.gyu.engdu.domain.user.application.dto.response.UserSummaryResponse;
 import com.gyu.engdu.domain.user.domain.User;
 import com.gyu.engdu.domain.user.domain.UserRepository;
@@ -26,5 +27,10 @@ public class UserQueryService {
   public UserSummaryResponse findSummaryUserInfo(Long userId) {
     User user = findExistingUser(userId);
     return UserSummaryResponse.fromEntity(user);
+  }
+
+  public UserDetailResponse findDetailUserInfo(Long userId) {
+    User user = findExistingUser(userId);
+    return UserDetailResponse.fromEntity(user);
   }
 }
