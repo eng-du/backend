b840d72f827edb884558df89ed1be1cb936fcf08
young999999999 <ssas146310@gmail.com>
2026-03-03 15:02:41 +0900

    style: 불필요한 파일, 컬ㄹ머 제거

--- src/main/java/com/gyu/engdu/domain/engdu/presentation/dto/request/CreateEngduContentRequest.java
+++
@@ -1,9 +0,0 @@
-package com.gyu.engdu.domain.engdu.presentation.dto.request;
-
-import com.gyu.engdu.domain.engdu.domain.enums.PartType;
-import jakarta.validation.constraints.NotNull;
-
-public record CreateEngduContentRequest(
-        @NotNull PartType step) {
-
-}
--- src/main/java/com/gyu/engdu/domain/engdu/presentation/dto/request/CreateEngduRequest.java
+++ src/main/java/com/gyu/engdu/domain/engdu/presentation/dto/request/CreateEngduRequest.java
@@ -2,7 +2,6 @@ package com.gyu.engdu.domain.engdu.presentation.dto.request;
 
 public record CreateEngduRequest(
     String topic,
-    Long engduId,
     String level
 ) {
 
