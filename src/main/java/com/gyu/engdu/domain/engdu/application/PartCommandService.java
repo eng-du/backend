fc91d58ddf0fad86e6caf236ad321f223d7a055e
young999999999 <ssas146310@gmail.com>
2026-03-05 11:55:14 +0900

    refactor: [리뷰 반영] PartType 매개변수 제거

--- src/main/java/com/gyu/engdu/domain/engdu/application/PartCommandService.java
+++ src/main/java/com/gyu/engdu/domain/engdu/application/PartCommandService.java
@@ -53,12 +53,12 @@ public class PartCommandService {
     }
 
     @Transactional
-    public void saveResultAndMarkDone(Long partId, PartType partType, GeneratedEngduResponse response) {
+    public void saveResultAndMarkDone(Long partId, GeneratedEngduResponse response) {
         Part part = partRepository.findById(partId)
                 .orElseThrow(() -> new PartNotFoundException(partId));
 
         // INITIAL 파트 저장 시 LLM이 생성한 타이틀을 Engdu에 반영합니다.
-        if (partType == PartType.INITIAL) {
+        if (part.getPartType() == PartType.INITIAL) {
             part.getEngdu().changeTitle(response.title());
         }
 
