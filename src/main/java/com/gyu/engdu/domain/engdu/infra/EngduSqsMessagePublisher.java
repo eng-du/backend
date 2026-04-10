2b6a7b605882775f6a52c1efac4b38bcedaf9492
young999999999 <ssas146310@gmail.com>
2026-04-10 19:23:33 +0900

    feat: try-finally 제거 및 @MdcContext 어노테이션 추가

--- src/main/java/com/gyu/engdu/domain/engdu/infra/EngduSqsMessagePublisher.java
+++ src/main/java/com/gyu/engdu/domain/engdu/infra/EngduSqsMessagePublisher.java
@@ -1,12 +1,12 @@
 package com.gyu.engdu.domain.engdu.infra;
 
+import com.gyu.engdu.config.MdcContext;
 import com.gyu.engdu.domain.engdu.application.EngduMessagePublisher;
 import com.gyu.engdu.domain.engdu.infra.dto.GenerateEngduPartMessage;
 import io.awspring.cloud.sqs.operations.SqsTemplate;
 import io.micrometer.core.annotation.Timed;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.slf4j.MDC;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
@@ -20,20 +20,15 @@ public class EngduSqsMessagePublisher implements EngduMessagePublisher {
   @Value("${spring.cloud.aws.sqs.queue-name}")
   private String queueName;
 
+  @MdcContext(key = "traceId", paramName = "traceId")
   @Timed("sqs")
   public void publish(GenerateEngduPartMessage message, String traceId) {
-    MDC.put("traceId", traceId);
+    sqsTemplate.send(to -> to
+        .queue(queueName)
+        .payload(message)
+        .header("traceId", traceId));
 
-    try {
-      sqsTemplate.send(to -> to
-          .queue(queueName)
-          .payload(message)
-          .header("traceId", traceId));
-
-      log.info("[SQS 메시지 발행] 메시지 발행에 성공했습니다. engduId={}, userId={}, step={}",
-          message.engduId(), message.userId(), message.step());
-    } finally {
-      MDC.remove("traceId");
-    }
+    log.info("[SQS 메시지 발행] 메시지 발행에 성공했습니다. engduId={}, userId={}, step={}",
+        message.engduId(), message.userId(), message.step());
   }
 }
