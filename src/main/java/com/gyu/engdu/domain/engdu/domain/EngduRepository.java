483ae6e04b5e28bd8c6f19c9b9fb4279df09916d
young999999999 <ssas146310@gmail.com>
2026-04-25 20:54:19 +0900

    feat: 커버링 인덱스를 이용한 조회 쿼리 추가

--- src/main/java/com/gyu/engdu/domain/engdu/domain/EngduRepository.java
+++ src/main/java/com/gyu/engdu/domain/engdu/domain/EngduRepository.java
@@ -1,16 +1,30 @@
 package com.gyu.engdu.domain.engdu.domain;
 
+import java.util.List;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface EngduRepository extends JpaRepository<Engdu, Long> {
 
-  Page<Engdu> findAllByUserIdAndIsAllSolved(Long userId, Boolean isAllSolved, Pageable pageable);
+  // covering index를 위해 id만 조회한다.
+  @Query("SELECT e.id FROM Engdu e WHERE e.userId = :userId AND e.isAllSolved = :isAllSolved")
+  Page<Long> findIdsByUserIdAndIsAllSolved(
+      @Param("userId") Long userId,
+      @Param("isAllSolved") Boolean isAllSolved,
+      Pageable pageable
+  );
 
-  Page<Engdu> findAllByUserId(Long userId, Pageable pageable);
+  // covering index를 위해 id만 조회한다.
+  @Query("SELECT e.id FROM Engdu e WHERE e.userId = :userId")
+  Page<Long> findIdsByUserId(@Param("userId") Long userId, Pageable pageable);
+
+  List<Engdu> findByIdIn(List<Long> ids, Sort sort);
 
   boolean existsByUserId(Long userId);
 }
