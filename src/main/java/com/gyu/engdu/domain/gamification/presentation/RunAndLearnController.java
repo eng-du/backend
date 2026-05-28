6c5171b7f1b470c9f68f3ccb4a2bd9483169c81c
young999999999 <ssas146310@gmail.com>
2026-05-28 15:30:18 +0900

    feat: 런앤런 문제 조회 api 개발

--- src/main/java/com/gyu/engdu/domain/gamification/presentation/RunAndLearnController.java
+++ src/main/java/com/gyu/engdu/domain/gamification/presentation/RunAndLearnController.java
@@ -1,13 +1,19 @@
 package com.gyu.engdu.domain.gamification.presentation;
 
 import com.gyu.engdu.domain.gamification.application.CreateRunAndLearnSessionService;
+import com.gyu.engdu.domain.gamification.application.RunAndLearnQueryService;
 import com.gyu.engdu.domain.gamification.application.StartRunAndLearnSessionService;
 import com.gyu.engdu.domain.gamification.application.dto.response.CreateRunAndLearnSessionResponse;
+import com.gyu.engdu.domain.gamification.application.dto.response.RunAndLearnQuestionResponse;
+import com.gyu.engdu.domain.gamification.presentation.dto.request.RunAndLearnQuestionRequest;
+import jakarta.validation.Valid;
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.concurrent.ThreadLocalRandom;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,11 +26,11 @@ public class RunAndLearnController {
 
     private final CreateRunAndLearnSessionService createRunAndLearnSessionService;
     private final StartRunAndLearnSessionService startRunAndLearnSessionService;
+    private final RunAndLearnQueryService runAndLearnQueryService;
 
     @PostMapping
     public ResponseEntity<CreateRunAndLearnSessionResponse> createSession(
-            @AuthenticationPrincipal(expression = "userId") Long userId
-    ) {
+            @AuthenticationPrincipal(expression = "userId") Long userId) {
         int seed = ThreadLocalRandom.current().nextInt();
         Long sessionId = createRunAndLearnSessionService.create(userId, seed);
 
@@ -34,10 +40,25 @@ public class RunAndLearnController {
     @PostMapping("/{sessionId}/start")
     public ResponseEntity<Void> startSession(
             @PathVariable("sessionId") Long sessionId,
-            @AuthenticationPrincipal(expression = "userId") Long userId
-    ) {
+            @AuthenticationPrincipal(expression = "userId") Long userId) {
         startRunAndLearnSessionService.start(userId, sessionId, LocalDateTime.now());
 
         return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/{sessionId}/question")
+    public ResponseEntity<List<RunAndLearnQuestionResponse>> getQuestions(
+            @PathVariable("sessionId") Long sessionId,
+            @AuthenticationPrincipal(expression = "userId") Long userId,
+            @Valid RunAndLearnQuestionRequest request) {
+
+        List<RunAndLearnQuestionResponse> response = runAndLearnQueryService.getQuestions(
+                userId,
+                sessionId,
+                request.startIndex(),
+                request.count()
+        );
+
+        return ResponseEntity.ok(response);
     }
 }
